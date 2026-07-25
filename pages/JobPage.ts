021e9fe9da797a6d9f338285dea8a3135e0518e9
Dzmitry Liashchynski <d.liashchynski@godeltech.com>
2026-07-25 14:36:47 +0200

    Created tests for Scenario 1-2, created POM for tests

---
+++ pages/JobPage.ts
@@ -0,0 +1,44 @@
+import { Locator, Page, expect } from '@playwright/test';
+
+export class JobPage {
+    readonly page: Page;
+    readonly jobHeading: Locator;
+    readonly saveJobButton: Locator;
+    readonly heartIconNumber: Locator;
+    readonly savedJobsDropdown: Locator;
+    readonly savedJobsTitle: Locator;
+
+    constructor (page: Page) {
+        this.page = page;
+        this.jobHeading = this.page.getByRole('heading', { level: 1 });
+        this.saveJobButton = this.page.locator('.js-save-job-btn').first();
+        this.heartIconNumber = this.page.locator('.saved-jobs-dropdown__number');
+        this.savedJobsDropdown = this.page.getByRole('button', {name: 'Saved jobs'});
+        this.savedJobsTitle = this.page.locator('.saved-jobs-dropdown__list-item');
+        
+    };
+
+    async checkJobHeading (title: string): Promise<void> {
+        await expect(this.jobHeading).toContainText(title);
+    };
+
+    async saveJob (): Promise<void> {
+        await this.saveJobButton.click();
+    };
+
+    async checkNumberOfSavedJobs(amount: string): Promise<void> {
+        await expect(this.heartIconNumber).toContainText(amount);
+    };
+
+    async clickSavedJobsDropdown(): Promise<void> {
+        await this.savedJobsDropdown.click();
+    };
+
+    async checkSavedJobTitle(title: string): Promise<void> {
+        await expect(this.savedJobsTitle).toContainText(title);
+    };
+
+
+
+    
+};
---
+++ testData.ts
@@ -0,0 +1,6 @@
+export const jobTitle = 'Manager';
+export const fallbackJobTitle = 'Designer';
+export const uniqueEmail = `test_${Date.now()}@mail.com`;
+export const jobCategory = 'IT & Digital Solutions';
+export const location = 'London, England';
+export const confirmMessage = 'Your subscription was submitted successfully.';
